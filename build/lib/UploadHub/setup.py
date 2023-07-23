93f024c8808b8597ce1ff2ca234f74267f1aed6b
TheCodingStudent <a360069@uach.mx>
2023-07-22 23:06:48 -0600

    Build 0.0.1

--- build/lib/UploadHub/setup.py
+++ build/lib/UploadHub/setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup, find_packages
-VERSION = '0.0.1'
-DESCRIPTION = 'PYPI and GitHub package uploader.'
-LONG_DESCRIPTION = 'Manages all the commands to upload a package to PYPI and GitHub repo.'
+VERSION = "VERSION"
+DESCRIPTION = "DESCRIPTION"
+LONG_DESCRIPTION = "DESCRIPTION"
 
 setup(
-    name='UploadHub',
+    name="NAME",
     version=VERSION,
-    author="Armando Chaparro",
-    author_email="<pylejandria@gmail.com>",
+    author="AUTHOR",
+    author_email="MAIL",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[
-        'twine',
-        'ttkbootstrap'
-    ],
+    install_requires=[],
     keywords=['python'],
     classifiers=[
         "Development Status :: 1 - Planning",
